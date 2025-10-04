2cbfc87cbd2c7c18a2b94f399e854945adce481c
Morningstar <39769523+sleepyheadlnd@users.noreply.github.com>
2025-10-04 14:51:38 +0200

    Create Resume Interface and update resume cards

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,43 @@
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import Navbar from "~/components/Navbar";
+import {resume} from "react-dom/server";
+import {resumes} from "../../constants";
+import ResumeCard from "~/components/ResumeCard";
+
+
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "Resume Analyzer" },
+    { name: "description", content: "Resume Analyzer using AI" },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  return <main className="bg-[url('/images/bg-main.svg')] bg-cover">
+      <Navbar/>
+        <section className="main-section">
+            <div className="page-heading">
+                <h1>The Perfect Tool To Boost Your Resume Ratings</h1>
+                <h2>Review and analyze your resume and get a detailed report with powerful AI feedback</h2>
+            </div>
+        </section>
+
+      {/*Check if resumes are available*/}
+      {resumes.length > 0 && (
+          <div className="resumes-section">
+              {
+               // Map resume
+               resumes.map((resume) => (
+                   <ResumeCard key ={resume.id} resume ={resume} />
+               ))
+              }
+          </div>
+      )
+
+      }
+
+
+
+      </main>;
 }
